ada16cab85e567f843f1cd449d51619716be16f2
damonliu <damon_liu@wistronits.com>
2020-12-11 14:20:12 +0800

    first commit

---
+++ eureka-server2/src/main/java/com/springcloud/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
