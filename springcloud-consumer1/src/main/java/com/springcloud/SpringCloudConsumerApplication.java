996005c1188de38802c58992843a67b1c00f9654
damonliu <damon_liu@wistronits.com>
2020-12-23 13:34:32 +0800

    hystrix & dashboard

--- springcloud-consumer1/src/main/java/com/springcloud/SpringCloudConsumerApplication.java
+++ springcloud-consumer1/src/main/java/com/springcloud/SpringCloudConsumerApplication.java
@@ -2,17 +2,21 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import com.springcloud.myRule.myRule;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RibbonClient(name = "springcloud-consumer1", configuration = myRule.class)
+@EnableHystrix
 public class SpringCloudConsumerApplication {
 
 	public static void main(String[] args) {
@@ -25,5 +29,15 @@ public class SpringCloudConsumerApplication {
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 
 }
--- springcloud-consumer1/src/main/java/com/springcloud/controller/ConsumerController.java
+++ springcloud-consumer1/src/main/java/com/springcloud/controller/ConsumerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping(path = "/consumer")
 public class ConsumerController {
@@ -13,9 +15,14 @@ public class ConsumerController {
 	@Autowired
 	private RestTemplate restTemp;
 
+	@HystrixCommand(fallbackMethod = "getDefaultResp") //发生熔断时，调用getDefaultResp方法
 	@RequestMapping(path = "/getConsumer", method = RequestMethod.GET)
 	public String getConsumer() {
 		String result = restTemp.getForObject("http://springcloud-provider1/provider/getProvider", String.class);
 		return result;
+	}
+	
+	public String getDefaultResp() {
+		return "发生熔断!!!";
 	}
 }
